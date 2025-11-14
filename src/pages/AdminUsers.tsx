052340475b2a1339864b8efb6a1fc4b807aea0b7
kyusikko <kokyusik91@naver.com>
2025-11-14 18:01:47 +0900

    fix : admin 유저 전체 조회 테스트

--- src/pages/AdminUsers.tsx
+++ src/pages/AdminUsers.tsx
@@ -1,8 +1,24 @@
+import type { AdminUser } from "@/services/admin-auth.service";
+import { callEdgeFunction } from "@/services/admin-shop.service";
+import { useQuery } from "@tanstack/react-query";
+
+async function getAllAdminUsers(): Promise<AdminUser[]> {
+  return callEdgeFunction("/admin-users-get-all", {
+    method: "GET",
+  });
+}
+
 export default function AdminUsers() {
+  // TODO: 어드민 유저 데이터를 불러오는 api 테스트
+  const { data, error } = useQuery({
+    queryKey: ["amin-users"],
+    queryFn: getAllAdminUsers,
+  });
+
   return (
     <div>
-      <h1 className='text-2xl font-bold mb-4'>어드민 유저 관리</h1>
-      <p className='text-gray-600'>어드민 유저를 관리하는 페이지입니다.</p>
+      <h1 className="text-2xl font-bold mb-4">어드민 유저 관리</h1>
+      <p className="text-gray-600">어드민 유저를 관리하는 페이지입니다.</p>
     </div>
   );
 }
